5bbfcfa3b1ff805920c55b501c856d800f0ae8cb
Massimiliano Gubinelli <m.gubinelli@gmail.com>
2024-04-30 23:40:16 +0100

    Fix formatting

--- tmfs-dir.scm
+++ tmfs-dir.scm
@@ -4,23 +4,27 @@
 ;; Do (load-document "tmfs://local-dir/Users/mgubi")
 
 (define (dir-document doc)
-  `(document
-     (TeXmacs ,(texmacs-version))
-     (style (tuple "generic"))
-     (body (document (hide-preamble (document  (assign "font-family" "tt") (assign "font-base-size" "8pt") (assign  "par-par-sep" "0.2fn") (assign "font" "Noto"))) 
-                 ,doc))))
+  (with preamble `(document  
+        (assign "font-family" "tt") (assign "font-base-size" "8pt") 
+        (assign  "par-par-sep" "0.2fn") (assign "font" "Noto"))
+    `(document
+       (TeXmacs ,(texmacs-version))
+       (style (tuple "generic"))
+       (body (document (hide-preamble ,preamble) ,doc)))))
 
 (define (dir-line dirname entry)
   (let* ((dir?        (url-directory? entry))
-         (url         (url->string (if dir? (url-append "tmfs://local-dir/" (url-unroot entry)) entry)))
-         (short-name  (string-append (url->string (url-delta dirname entry)) (if dir? "..." "")))
+         (url         (url->string (if dir? (url-append "tmfs://local-dir/" (url-unroot entry)) 
+                                            entry)))
+         (short-name  (string-append (url->string (url-delta dirname entry)) 
+                                     (if dir? "..." "")))
          (hlink `(hlink ,short-name ,url)))
       hlink))
 
 (define (dir-page name entries)
   (dir-document `(document (section* ,(string-append "File list " (url->string name)))
-                              (with "par-columns" "2" (document  ,@(map (cut dir-line name <>) entries))))))
-
+                           (with "par-columns" "2" 
+                              (document  ,@(map (cut dir-line name <>) entries))))))
 
 (tmfs-load-handler (local-dir name)
  (with url  (url-append "/" (url-append name "./"))
